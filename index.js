f9a594bf0b04a4c37b39526659ddc3f384ee5b1a
Enock-Yegon <ykibet70@gmail.com>
2022-12-08 17:36:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function hasTargetSum(array, target) {
+//function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+//}
 
 /* 
   Write the Big O time complexity of your function here
@@ -15,18 +15,27 @@ function hasTargetSum(array, target) {
 */
 
 // You can run `node index.js` to view these console logs
+function hasTargetSum(array, target) {
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
+}
+//console.log(twoSum([15, 7, 11, 2],9))
+//console.log(twoSum([3, 2, 4],6))
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
